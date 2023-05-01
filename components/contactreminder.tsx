0a01e1938709ccba8ce393fab37c737fba8279bb
Johann Mendoza <johannmendoza1209@gmail.com>
2023-05-01 16:08:20 +0800

    transition only on biggwe viewports

--- components/contactreminder.tsx
+++ components/contactreminder.tsx
@@ -1,9 +1,17 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ContactReminder = () => {
   const [restrictLeave, setRestrictLeave] = useState(false);
+  const [isWidthMobile, setIsWidthMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsWidthMobile(true);
+    }
+  }, []);
+
   return (
     <motion.div
       className="flex justify-center"
@@ -15,53 +23,74 @@ export const ContactReminder = () => {
         <h1 className="w-fit shrink font-playfairDisplay text-lg sm:text-2xl">
           Have I Piqued your Interest?
         </h1>
-        <div
-          className="flex gap-4"
-          onMouseEnter={() => setRestrictLeave(true)}
-          onMouseLeave={() => setRestrictLeave(false)}
-        >
-          <motion.button
-            initial={{ opacity: 1, x: 0 }}
-            whileHover={{ opacity: 0, x: 40 }}
-            whileTap={{ scale: 0.9, x: 0 }}
-            className="button_transition hidden border-2 px-4 py-2 md:flex"
+
+        {isWidthMobile ? (
+          <div
+            className="flex gap-4"
+            onMouseEnter={() => setRestrictLeave(true)}
+            onMouseLeave={() => setRestrictLeave(false)}
           >
-            Leave
-          </motion.button>
-          <AnimatePresence
-            initial={false}
-            mode="wait"
+            <motion.div
+              className="flex"
+              whileTap={{ scale: 0.9 }}
+            >
+              <Link
+                href="/contact"
+                className="button_transition border-2 border-white bg-white px-4 py-2 text-center text-sm text-blue-500 hover:border-blue-400"
+              >
+                Contact Me
+              </Link>
+            </motion.div>
+          </div>
+        ) : (
+          <div
+            className="flex gap-4"
+            onMouseEnter={() => setRestrictLeave(true)}
+            onMouseLeave={() => setRestrictLeave(false)}
           >
-            {restrictLeave ? (
-              <motion.div
-                className="flex shrink-0"
-                initial={{ x: 0 }}
-                animate={{ x: -100 }}
-                exit={{ x: 0 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <Link
-                  href="/contact"
-                  className="button_transition border-2 border-white bg-white px-4 py-2 text-sm text-blue-500 hover:border-blue-400"
+            <motion.button
+              initial={{ opacity: 1, x: 0 }}
+              whileHover={{ opacity: 0, x: 40 }}
+              whileTap={{ scale: 0.9, x: 0 }}
+              className="button_transition hidden border-2 px-4 py-2 md:flex"
+            >
+              Leave
+            </motion.button>
+            <AnimatePresence
+              initial={false}
+              mode="wait"
+            >
+              {restrictLeave ? (
+                <motion.div
+                  className="flex shrink-0"
+                  initial={{ x: 0 }}
+                  animate={{ x: -100 }}
+                  exit={{ x: 0 }}
+                  whileTap={{ scale: 0.9 }}
                 >
-                  Contact Me
-                </Link>
-              </motion.div>
-            ) : (
-              <motion.div
-                className="flex"
-                whileTap={{ scale: 0.9 }}
-              >
-                <Link
-                  href="/contact"
-                  className="button_transition border-2 border-white bg-white px-4 py-2 text-center text-sm text-blue-500 hover:border-blue-400"
+                  <Link
+                    href="/contact"
+                    className="button_transition border-2 border-white bg-white px-4 py-2 text-sm text-blue-500 hover:border-blue-400"
+                  >
+                    Contact Me
+                  </Link>
+                </motion.div>
+              ) : (
+                <motion.div
+                  className="flex"
+                  whileTap={{ scale: 0.9 }}
                 >
-                  Contact Me
-                </Link>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </div>
+                  <Link
+                    href="/contact"
+                    className="button_transition border-2 border-white bg-white px-4 py-2 text-center text-sm text-blue-500 hover:border-blue-400"
+                  >
+                    Contact Me
+                  </Link>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        )}
       </div>
     </motion.div>
   );
